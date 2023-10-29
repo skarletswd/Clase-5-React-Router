34fc2d9b892ac750ca019bf1faf214d2d67c613f
Andrea Lopez <skarletswdev@gmail.com>
2023-10-29 17:38:00 -0300

    configure router en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Inicio from "./Inicio"
+import NotFound from "./NotFound"
+
 function App() {
 
-  return <>App</>
+  return (
+    <BrowserRouter>
+ 
+      <Routes>
+
+        <Route path="/" Component={Inicio}/>
+        <Route path="*" Component={NotFound}/>
+
+      </Routes>
+
+    </BrowserRouter>
+  )
 }
 
 export default App
